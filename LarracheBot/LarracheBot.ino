c6d8bc0371cab827c54150902ab281b55504ccbc
Wizado <nicolas.chatelat@u-psud.fr>
2017-05-17 18:22:14 +0200

    Creation projet LarracheBot.

---
+++ LarracheBot/LarracheBot.ino
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "config.h"
+
+int main(void)
+{
+
+  for(;;)
+  {
+    //
+  }
+
+  return 0;
+}
+
---
+++ LarracheBot/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+#define VERSION    "v0.1A\n"
+
+#endif /* CONFIG_H_ */
